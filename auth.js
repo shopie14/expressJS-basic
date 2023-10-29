a7854cdc9a4db8380939a1a51a77c3fb9693a0b7
shopie14 <sh.pinh14@gmail.com>
2023-10-29 21:13:08 +0700

    ui login and signup but not successful login

--- auth.js
+++ auth.js
@@ -4,37 +4,67 @@ const path = require("path");
 const hbs = require("hbs");
 const collection = require("./models/users_auth");
 const viewPath = path.join(__dirname, "./views");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
 
 app.use(express.json());
 app.set("view engine", "hbs");
 app.set("views", viewPath);
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// get login
+function getHashedPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hash = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+    .toString("hex");
+  return {
+    salt,
+    hash,
+  };
+}
+
 app.get("/", (req, res) => {
   res.render("login");
 });
 
-// get signup
 app.get("/signup", (req, res) => {
-  res.render("signup");
+  res.render("signup", {
+    message: "Signup successful",
+    messageClass: "alert-success",
+  });
 });
 
 app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = getHashedPassword(password);
+
   const data = {
-    email: req.body.email,
-    password: req.body.password,
+    email,
+    password: hashedPassword.hash,
   };
 
-  await collection.insertMany([data]);
+  await collection.create(data);
   res.render("home");
 });
 
 app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const check = await collection.findOne({ email: req.body.email });
-    if (check.password === req.body.password) {
-      res.render("home");
+    const check = await collection.findOne({ email });
+    if (!check) {
+      res.send("Wrong details");
+      return;
+    }
+
+    const hashedPassword = getHashedPassword(password);
+
+    if (check.hash === hashedPassword.hash) {
+      res.render("login", {
+        message: "Login successful",
+        messageClass: "alert-success",
+      });
     } else {
       res.send("Wrong password");
     }
